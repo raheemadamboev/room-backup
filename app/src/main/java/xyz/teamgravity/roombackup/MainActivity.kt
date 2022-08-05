d035f5a73d9ca42ff8c59c94e0652b7a0abfde5c
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-05 18:50:03 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/roombackup/MainActivity.kt
@@ -0,0 +1,25 @@
+package xyz.teamgravity.roombackup
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.roombackup.ui.theme.RoomBackupTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoomBackupTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
