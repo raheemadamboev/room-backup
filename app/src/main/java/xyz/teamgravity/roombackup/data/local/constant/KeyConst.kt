3022c722bc9d45e7f35fbf85a775e0f00a35dc36
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-05 19:59:54 +0500

    add key database constants

---
+++ app/src/main/java/xyz/teamgravity/roombackup/data/local/constant/KeyConst.kt
@@ -0,0 +1,21 @@
+package xyz.teamgravity.roombackup.data.local.constant
+
+object KeyConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "keys_database"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table keys
+     * Entity -> [xyz.teamgravity.roombackup.data.local.entity.KeyEntity]
+     * Model -> [xyz.teamgravity.roombackup.data.model.KeyModel]
+     */
+    const val TABLE_KEY = "table_key"
+}
