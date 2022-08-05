a67618572cc5ab3c7b384262260a6350ab966956
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-05 20:52:01 +0500

    setup main for injection

--- app/src/main/java/xyz/teamgravity/roombackup/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/roombackup/presentation/activity/Main.kt
@@ -7,9 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.roombackup.presentation.screen.KeyListScreen
 import xyz.teamgravity.roombackup.presentation.theme.RoomBackupTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,6 +22,7 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    KeyListScreen()
                 }
             }
         }
